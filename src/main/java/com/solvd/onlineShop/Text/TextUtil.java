3464bba02cbf6eee953007f1d56d29bdf25d165a
eliolias <65453595+eliolias@users.noreply.github.com>
2022-08-22 09:03:28 -0400

    fixed text util file

--- src/main/java/com/solvd/onlineShop/Text/TextUtil.java
+++ src/main/java/com/solvd/onlineShop/Text/TextUtil.java
@@ -1,5 +1,6 @@
 package com.solvd.onlineShop.Text;
 
+import java.nio.charset.Charset;
 import java.util.*;
 import java.io.File;
 import java.io.IOException;
@@ -13,33 +14,31 @@ import org.apache.logging.log4j.Logger;
 public class TextUtil {
     private final static Logger LOGGER = LogManager.getLogger(TextUtil.class);
 
-
-    public static void processFile() throws IOException {
+    public static void processFile(String countedWord) throws IOException {
         File readFile = new File("src/main/resources/article.txt");
         File writeFile = new File("src/main/resources/result.txt");
         String fileContents = FileUtils.readFileToString(readFile, StandardCharsets.UTF_8.name());
         List<String> fileWords = Arrays.asList(fileContents.split(" "));
-        //Could use a set to avoid duplication??
         List<String> lines = new ArrayList<>();
-        for (int i = 0; i < fileWords.size(); i++) {
-            int count = 0;
-            String temp = null;
-            for (int j = 0; j < fileWords.size(); j++) {
-                if (fileWords.get(i).equals(fileWords.get(j))) {
-                    temp = fileWords.get(i);
-                    count++;
-                }
-            }
-            if (count > 1) {
-                lines.add(temp + "  " + count);
-            }
+        long count = fileWords.stream().filter(word -> {
+            String fixedWord = word
+                    .replaceAll("!", "")
+                    .replaceAll("`", "")
+                    .replaceAll(",", "")
+                    .replaceAll("'", "")
+                    .replaceAll("\\.", "")
+                    .replaceAll("\\?", "")
+                    .toLowerCase();
+            return fixedWord.equals(countedWord.toLowerCase());
 
-        }
-        FileUtils.writeLines(writeFile, lines);
+
+        }).count();
+        String result = countedWord + " - " + count;
+        FileUtils.writeStringToFile(writeFile, result, (Charset) null);
         LOGGER.info("WRITING");
     }
 
     public static void main(String[] args) throws IOException {
-        TextUtil.processFile();
+        TextUtil.processFile("it");
     }
 }
